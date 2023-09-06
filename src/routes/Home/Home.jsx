3f0ed976c5012848dcba2e9b8e10b0743110f8c3
calinathalya <calina.thalya@icloud.com>
2023-09-06 01:06:05 -0300

    arrumando a pagina home

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,8 +1,7 @@
-export default function Error() {
-    return (
-      <>
-          <h1>Erro 404 - Página não encontrada</h1>    
-      </>
-    )
-  }
-  
+export default function Home() {
+  return (
+    <main>
+        <h1>HOME</h1>
+    </main>
+  )
+}
