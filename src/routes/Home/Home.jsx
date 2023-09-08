8e1f3bbb1de92f5b3a50c95c72c242e961754237
calinathalya <calina.thalya@icloud.com>
2023-09-07 22:56:13 -0300

    fazendo alteração na pagina home

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -2,6 +2,17 @@ export default function Home() {
   return (
     <main>
         <h1>HOME</h1>
+        <div className="promocao">
+        <img src="src/routes/Home/foto1.png" alt="Promoção 1" />
+        <p>Descrição da Promoção 1</p>
+      </div>
+
+       
+      <div className="promocao">
+        <img src="src/routes/Home/Home.jsx" alt="Promoção 2" />
+        <p>Descrição da Promoção 2</p>
+      </div>
+
     </main>
   )
 }
