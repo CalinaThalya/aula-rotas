7bf4140de5b7948736edd4817a8498fda5082f27
calinathalya <calina.thalya@icloud.com>
2023-09-08 22:20:53 -0300

    atualizando

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
           <h1>HOME</h1>
           <div>
           <img src="https://github.com/CalinaThalya/aula-rotas/blob/0b8696b009a34b5d47272697c978f7e32b193e0e/public/fotooo.jpg" alt="MEGA PROMOÇÃO"/>
-          <p>Compre no boleto ou parecaldo no cartão de credito</p>
+          <p>Compre no boleto em 12 vezes, ou parcelado no cartão de credito</p>
           </div>
       </main>
     )
