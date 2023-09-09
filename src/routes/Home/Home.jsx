3237597c58d84566a046fad9c59f81b011e76ae9
calinathalya <calina.thalya@icloud.com>
2023-09-09 01:02:41 -0300

    atualizando o home

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,11 +1,19 @@
 export default function Home() {
     return (
       <main>
-          <h1>HOME</h1>
-          <div>
-          <img src="https://github.com/CalinaThalya/aula-rotas/blob/0b8696b009a34b5d47272697c978f7e32b193e0e/public/fotooo.jpg" alt="MEGA PROMOÇÃO"/>
-          <p>Compre no boleto em 12 vezes ou parcelado no cartão de credito</p>
-          </div>
-      </main>
-    )
-  }
+      <div>
+        <head>
+        <h1>HOME</h1>
+        </head>
+           </div>
+      <div className="image-container">
+        <img src="/foto1.webp" alt="MEGA PROMOÇÃO" />
+        <img src="/foto3.webp" alt="Outra imagem" />
+      </div>
+      <div className="text-container">
+      </div>
+    </main>
+  );
+}
+    
+  
