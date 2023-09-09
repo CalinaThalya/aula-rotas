57d57867452d5f8cc69960038a96eaf46bca05e8
calinathalya <calina.thalya@icloud.com>
2023-09-09 17:24:26 -0300

    pagina error

--- src/routes/Error/Error.jsx
+++ src/routes/Error/Error.jsx
@@ -1,7 +1,9 @@
 export default function Error() {
   return (
     <>
-        <h1>Erro 404 - Página não encontrada</h1>    
-    </>
-  )
+    <h1>Erro 404 - Página não encontrada</h1>
+    <p>A página que você está procurando não existe.</p>
+    <Link to="/src/routes/Home/Home.jsx">Voltar para a página inicial</Link>
+  </>
+);
 }
