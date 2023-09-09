d64f51d28405607d75cbda58c6e78288994ff47e
calinathalya <calina.thalya@icloud.com>
2023-09-08 21:35:50 -0300

    arrumando

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -3,14 +3,8 @@ export default function Home() {
     <main>
         <h1>HOME</h1>
         <div className="promocao">
-        <img src="/home/calina/aula-rotas/aula-rotas/src/routes/Home/fotooo.jpg" alt="Promoção" />
-        <p>Descrsrc/routes/Home/fotooo.jpgição da Promoção 1</p>
-      </div>
-
-       
-      <div className="promocao">
-        <img src="src/routes//foto2.png" alt="Promoção 2" />
-        <p>Descrição da Promoção 2</p>
+        <img src="/src/routes/fotoo.jpg" alt="Promoção 1" />
+        <p>Descrição da Promoção 1</p>
       </div>
 
     </main>
