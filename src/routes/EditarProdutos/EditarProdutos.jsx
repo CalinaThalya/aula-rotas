c050a37abd90f3afa3c3c0f465035f090c7ec9dd
calinathalya <calina.thalya@icloud.com>
2023-09-10 21:13:58 -0300

    finalizando :)

--- src/routes/EditarProdutos/EditarProdutos.jsx
+++ src/routes/EditarProdutos/EditarProdutos.jsx
@@ -2,16 +2,21 @@ import {useParams, useNavigate } from "react-router-dom"
 import { ListaProdutos } from "../../components/ListaProduto"
 
 export default function EditarProduto() {
+  // Obtém a lista de produtos e funções de navegação do React Router
 
   const lista = ListaProdutos
   const navegacao = useNavigate()
+    // Obtém o parâmetro 'id' da URL usando o hook 'useParams'
   const {id} = useParams()
-
+  // Filtra a lista de produtos para encontrar o produto com o 'id' correspondente
   const proc = lista.filter(prod => prod.id == id)
+   // Pega o primeiro produto encontrado (ou undefined se não encontrado)
   const produto = proc[0]
-  
+    // Função para salvar o produto (simula uma ação de salvar)
+        // Exibe um alerta com o nome do produto
   const salvar = () => {
     alert(`Produto: ${produto.nome} salvo com sucesso!`)
+        // Redireciona de volta para a página de produtos
     return navegacao('/produtos')
   }
   
--- src/routes/Produtos/Produtos.jsx
+++ src/routes/Produtos/Produtos.jsx
@@ -7,9 +7,12 @@ export default function Produtos() {
   
     <main>
       <h1>Produtos</h1>
+            {/* Mapeia a lista de produtos e renderiza um link para visualizar cada produto */}
       {ListaProdutos.map(prod => (
         <div key={prod.id}>
+                    {/* Link para a página de edição do produto com base no 'id' */}
           <Link to={`/produtos/editar/${prod.id}`}>Visualizar o produto: {prod.nome}
+                      {/* Exibe o texto 'Visualizar o produto' seguido pelo nome do produto */}
           </Link>
         </div>
       ))}
