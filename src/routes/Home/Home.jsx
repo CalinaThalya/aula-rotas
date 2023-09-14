43099fecd7672d4e542e856ce814172a2ec7a431
calinathalya <calina.thalya@icloud.com>
2023-09-14 02:58:56 -0300

    FInalizado ;)

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
 
       <main>
-      <h1>SUPER PROMOÇÃO: Iphone 13 ou ipad com 60% off! veja na pagina de produtos!</h1>
+      <h1>SUPER PROMOÇÃO: Iphone 13 ou ipad com 60% off! veja na página de produtos!</h1>
       <div className="image-container">
           <img src="/images (1).jpeg" alt="MEGA PROMOÇÃO" />
           <img src="/images (2).jpeg" alt="promoção" />
